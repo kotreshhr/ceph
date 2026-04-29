17fcdc94f3e351aaa9108ba0e8614cae1907ab34
Kotresh HR <khiremat@redhat.com>
2026-04-29 14:49:15 +0530

    mgr/mirroring: Add new interface to expose mirroring metrics
    Fixes: https://tracker.ceph.com/issues/73453
    Signed-off-by: Kotresh HR <khiremat@redhat.com>
    (cherry picked from commit 7b4571473f55d077e5112a0a64199b24c3acb820)

--- src/pybind/mgr/mirroring/module.py
+++ src/pybind/mgr/mirroring/module.py
@@ -116,3 +116,10 @@ class Module(MgrModule):
                                       format: str = 'json'):
         """Get mirror daemon status"""
         return self.fs_snapshot_mirror.daemon_status(format)
+
+    @MirroringCLICommand.Read('fs snapshot mirror status')
+    def snapshot_mirror_status(self,
+                               fs_name: str,
+                               mirrored_dir_path: str):
+        """Get snapshot mirror metrics status for a mirrored directory"""
+        return self.fs_snapshot_mirror.metrics_status(fs_name, mirrored_dir_path)
